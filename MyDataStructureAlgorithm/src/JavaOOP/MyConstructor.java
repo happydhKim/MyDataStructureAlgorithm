2fdfd8885cf66200e0b26ebdc2bea6eb821836b1
happydhKim <happytime870616@gmail.com>
2018-06-07 22:36:58 +0900

    Java Constructor

---
+++ MyDataStructureAlgorithm/src/JavaOOP/MyConstructor.java
@@ -0,0 +1,25 @@
+package JavaOOP;
+
+/**
+ * 
+ * @author kimdohyun 생성자생성자의 이름은 클래스의 이름과 같아야 한다. 생성자는 리턴 값이 없다.
+ * 
+ **/
+
+class Pair {
+	int x;
+	int y;
+
+	Pair() { // 매개변수가 없는 생성자.
+
+	}
+
+	Pair(int x, int y) {// 매개변수가 있는 생성자.
+		this.x = x;
+		this.y = y;
+	}
+}
+
+public class MyConstructor {
+
+}
