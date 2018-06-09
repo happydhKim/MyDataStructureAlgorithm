3bd1d922708faa50516c002021a70607cf6fa008
happydhKim <happytime870616@gmail.com>
2018-06-09 22:01:46 +0900

    set bit

--- MyDataStructureAlgorithm/SamsungExpertAcademy/class1problemSolution/bit.java
+++ MyDataStructureAlgorithm/SamsungExpertAcademy/class1problemSolution/bit.java
@@ -2,9 +2,33 @@ package class1problemSolution;
 
 /**
  * 
- * @author kimdohyun
- * 
+ * @author kimdohyun 비트 연산자는 다른 연산자에 비해서 실행 시간이 적게 소요됨 연산 속도가 좋으며, 메모리 절약이 가능하다.
+ *         &, |, ^, ~, <<, >> 대표적으로 짝홀 판단 해주는 모듈러 연산자 (N % 2) 가 있다. 마지막 비트가 1인지
+ *         0인지 판단 할 수 있는 (N & 1) 가 있다.
  **/
 public class bit {
+	private static void bitAnd() { // &
 
+	}
+
+	private static void bitOr() { // |
+
+	}
+
+	private static void bitXor() { // ^
+
+	}
+
+	private static void bitReverse() { // ~
+
+	}
+
+	private static void bitLeftShift() { // << 곱한결과 
+		//1<<n = 2의 n승을 뜻함 원소가 n 개인 경우의 모든 부분집합의 수를 말함 
+
+	}
+
+	private static void bitRightShift() { // >>
+
+	}
 }
