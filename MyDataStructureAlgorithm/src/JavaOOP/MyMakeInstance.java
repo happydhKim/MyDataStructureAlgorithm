3dc4ed57b67fadd9f3661e6c02ab2db835952de0
happydhKim <happytime870616@gmail.com>
2018-06-17 23:22:24 +0900

    this OOP

--- MyDataStructureAlgorithm/src/JavaOOP/MyMakeInstance.java
+++ MyDataStructureAlgorithm/src/JavaOOP/MyMakeInstance.java
@@ -8,6 +8,14 @@ class Car {
 }
 
 public class MyMakeInstance {
+	private static String name;
+	private static double salary;
+
+	private void raseSalary(double byPercent) {
+		double raise = this.salary * byPercent / 100;
+		this.salary += raise;
+	}
+
 	public static void main(String args[]) {
 		Car c; // Car인스턴스를 참조하기 위한 변수 c를 생성 이 상태에서 참조변수로 아무것도 할 수 없다.
 		c = new Car(); // Car인스턴스를 생성한다. new 에 의해 Car클래스의 인스턴스가 메모리의 빈 공간에 생성된다.
@@ -15,14 +23,14 @@ public class MyMakeInstance {
 		c.gear(); // Car인스턴스의 메서드 gear()를 호출한다.
 		System.out.println("기어를" + c.gear + "단으로 변경합니다.");
 		System.out.println();
-		
-		Car arrCar[] = new Car[6]; //객체 배열의 활용
-		for (int i = 1; i <=5; i++) {
+
+		Car arrCar[] = new Car[6]; // 객체 배열의 활용
+		for (int i = 1; i <= 5; i++) {
 			arrCar[i] = new Car();
 			arrCar[i].gear = i;
 			arrCar[i].gear();
 			System.out.println("기어를" + arrCar[i].gear + "단으로 변경합니다.");
 		}
-		
+
 	}
 }
