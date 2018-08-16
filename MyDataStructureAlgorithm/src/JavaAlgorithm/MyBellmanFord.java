4fefc792934eac47f57468f6ee9c02789a2b6d0f
happydhKim <happytime870616@gmail.com>
2018-08-16 21:44:15 +0900

    Bellmanford

---
+++ MyDataStructureAlgorithm/src/JavaAlgorithm/MyBellmanFord.java
@@ -0,0 +1,65 @@
+package JavaAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author kimdohyun
+ * BOJ 11667 타임머신
+ */
+class TimeM {
+	int from, to, cost;
+
+	TimeM(int from, int to, int cost) {
+		this.from = from;
+		this.to = to;
+		this.cost = cost;
+	}
+}
+
+public class MyBellmanFord {
+	static final int inf = 100000000;
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int dist[] = new int[n + 1];
+		ArrayList<TimeM> a = new ArrayList<TimeM>();
+		for (int i = 0; i < m; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			int z = sc.nextInt();
+			a.add(new TimeM(x, y, z));
+		}
+
+		for (int i = 1; i <= n; i++) {
+			dist[i] = inf;
+		}
+		dist[1] = 0;
+		boolean negative_cycle = false;
+		for (int i = 1; i <= n; i++) {
+			for (TimeM e : a) {
+				int x = e.from;
+				int y = e.to;
+				int z = e.cost;
+				if (dist[x] != inf && dist[y] > dist[x] + z) {
+					dist[y] = dist[x] + z;
+					if (i == n) {
+						negative_cycle = true;
+					}
+				}
+			}
+		}
+		if (negative_cycle) {
+			System.out.println("-1");
+		} else {
+			for (int i = 2; i <= n; i++) {
+				if (dist[i] == inf)
+					dist[i] = -1;
+				System.out.println(dist[i]);
+			}
+		}
+	}
+}
--- MyDataStructureAlgorithm/src/JavaAlgorithm/MyFloydWarshall.java
+++ MyDataStructureAlgorithm/src/JavaAlgorithm/MyFloydWarshall.java
@@ -1,5 +1,18 @@
 package JavaAlgorithm;
 
 public class MyFloydWarshall {
+	private static int vertexNum;
+	private static double adj[][];
 
+	private static void floydWarshall() {
+		for (int i = 0; i < vertexNum; i++)
+			adj[i][i] = 0; // 자기 자신으로 가는 가중치는 0
+		for (int k = 0; k < vertexNum; k++) {
+			for (int i = 0; i < vertexNum; i++) {
+				for (int j = 0; j < vertexNum; j++) {
+					adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+				}
+			}
+		}
+	}
 }
