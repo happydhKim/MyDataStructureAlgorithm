ff83097d047f502bda371464764aa1ba9a89aa15
happydhKim <happytime870616@gmail.com>
2018-10-19 23:48:03 +0900

    quick sort add info

--- MyDataStructureAlgorithm/src/kakao/exam01.java
+++ MyDataStructureAlgorithm/src/kakao/exam01.java
@@ -1,5 +1,15 @@
 package kakao;
 
-public class exam01 {
+import java.util.Scanner;
 
+public class exam01 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int t = sc.nextInt();
+		int m = sc.nextInt();
+		int p = sc.nextInt();
+		
+		
+	}
 }
