bf7f6178556741e3cf3544f928e53034401c9e84
DHKIM <happytime870616@gmail.com>
2019-03-11 23:51:37 +0900

    DataStructure 1. JAVA class object

--- newMyDataStructureAlgorithm/java/oop/main.java
+++ newMyDataStructureAlgorithm/java/oop/main.java
@@ -1,5 +1,13 @@
 class main {
   private static final int N = 4;
+
+  public static Result query(int guess[]) {
+		Result result = new Result();
+
+    //code..
+
+		return result;
+	}
   public static void main(String args[]) {
     SubSolution subsolution = new SubSolution();
     int guess[] = new int[N];
--- newMyDataStructureAlgorithm/java/oop/sub.java
+++ newMyDataStructureAlgorithm/java/oop/sub.java
@@ -2,6 +2,7 @@ class SubSolution {
   public final static int N = 4;
 
 	public void doSubSolution(int guess[]) {
-
+    main.Result result = new main.Result();
+    Solution.query(guess);
 	}
 }
