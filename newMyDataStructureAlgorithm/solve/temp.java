6f18a0da28639de62a5324a58feff0265ff1c1ac
DHKIM <happytime870616@gmail.com>
2019-03-24 23:15:40 +0900

    algorithm 1.temp  update

--- newMyDataStructureAlgorithm/solve/temp.java
+++ newMyDataStructureAlgorithm/solve/temp.java
@@ -1,105 +1,89 @@
+package onlineStudy;
+
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
-public class temp {
-	static int D, W, K, cnt, aCnt, bCnt, ans, a[][], temp[][], step[];
+class Fish {
+	int x;
+	int y;
+	int step;
 
-	static void init() {
-		for (int i = 0; i < D; i++) {
-			for (int j = 0; j < W; j++) {
-				temp[i][j] = a[i][j];
-			}
-		}
+	Fish(int x, int y, int step) {
+		this.x = x;
+		this.y = y;
+		this.step = step;
 	}
+}
 
-	static boolean check() {
-		boolean chk = true;
-		for (int i = 0; i < W; i++) {
-			aCnt = 0;
-			bCnt = 0;
-			if (!chk) {
-				return false;
+public class goDs {
+	static final int X[] = { -1, 0, 0, 1 }; // 상 좌 우 하
+	static final int Y[] = { 0, -1, 1, 0 };
+
+	static int N, a[][], dist[][], age, cnt, eat, ans;
+	static boolean check[][], door;
+	static Queue<Fish> q;
+	static Queue<Fish> temp;
+
+	static void bfs() {
+		door = false;
+		while (true) {
+			int size = q.size();
+			if (size == 0) {
+				return;
 			}
-			for (int j = 0; j < D; j++) {
-				if (temp[j][i] == 0) {
-					bCnt = 0;
-					aCnt++;
-					if (aCnt == K) {
-						chk = true;
-						break;
-					} else {
-						chk = false;
+			for (int i = 0; i < size; i++) {
+				Fish f = q.remove();
+				int x = f.x;
+				int y = f.y;
+				int step = f.step;
+				for (int k = 0; k < 4; k++) {
+					int nx = x + X[k];
+					int ny = y + Y[k];
+					if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+						continue;
 					}
-				} else {
-					aCnt = 0;
-					bCnt++;
-					if (bCnt == K) {
-						chk = true;
-						break;
-					} else {
-						chk = false;
+					if (!check[nx][ny] && a[nx][ny] != 0 && a[nx][ny] < age) {
+						if (door && cnt < step) {
+							break;
+						}
+						temp.add(new Fish(nx, ny, step + 1));
+						check[nx][ny] = true;
+						door = true;
+						cnt = step;
 					}
-				}
-			}
-		}
-		return true;
-	}
-
-	static void go() {
-		for (int k = 0; k < D; k++) {
-			if (step[k] == 0) {
-				continue;
-			} else {
-				for (int i = 0; i < W; i++) {
-					temp[k][i] = step[k] - 1;
-				}
-			}
-		}
-
-	}
-
-	static void dfs(int depth) {
-		if (depth == D) {
-			init();
-			go();
-			cnt = 0;
-			if (check()) {
-				for (int i = 0; i < D; i++) {
-					if (step[i] != 0) {
-						cnt++;
+					if ((!check[nx][ny] && a[nx][ny] == 0) || (!check[nx][ny] && a[nx][ny] == age)) {
+						q.add(new Fish(nx, ny, step + 1));
+						check[nx][ny] = true;
 					}
 				}
-				if (ans > cnt) {
-					ans = cnt;
-				}
 			}
-			return;
 		}
-		for (int i = 0; i < 3; i++) {
-			step[depth] = i;
 
-			dfs(depth + 1);
-		}
 	}
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for (int TC = 1; TC <= T; TC++) {
-			D = sc.nextInt();
-			W = sc.nextInt();
-			K = sc.nextInt();
-			step = new int[D];
-			a = new int[D][W];
-			ans = 987654321;
-			temp = new int[D][W];
-			for (int i = 0; i < D; i++) {
-				for (int j = 0; j < W; j++) {
-					a[i][j] = sc.nextInt();
+		N = sc.nextInt();
+		a = new int[N][N];
+		dist = new int[N][N];
+		check = new boolean[N][N];
+		q = new LinkedList<>();
+		temp = new LinkedList<>();
+		age = 2;
+		ans = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				a[i][j] = sc.nextInt();
+				if (a[i][j] == 9) {
+					a[i][j] = 0;
+					q.add(new Fish(i, j, 0));
+					check[i][j] = true;
 				}
 			}
-			dfs(0);
-			System.out.println("#" + TC + " " + ans);
 		}
+		bfs();
+		System.out.println(ans);
 		sc.close();
 	}
 }
