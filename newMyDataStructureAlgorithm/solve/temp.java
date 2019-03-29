a8cdffe735323045f79375b98935ff0c0619b5c7
DESKTOP-TB730O5\happy <happytime870616@gmail.com>
2019-03-29 18:23:00 +0900

    Algorithm 1. update temp bfs

--- newMyDataStructureAlgorithm/solve/temp.java
+++ newMyDataStructureAlgorithm/solve/temp.java
@@ -1,159 +1,105 @@
+package dss;
+
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 
-class Tree implements Comparable<Tree> {
+class Pair {
 	int x;
 	int y;
-	int age;
-	String life;
 
-	Tree(int x, int y, int age, String life) {
+	Pair(int x, int y) {
 		this.x = x;
 		this.y = y;
-		this.age = age;
-		this.life = life;
-	}
-
-	@Override
-	public int compareTo(Tree t) {
-		return this.age > t.age ? 1 : -1;
 	}
 }
 
-public class test02 {
-	static final int X[] = { 0, 0, -1, 1, -1, 1, -1, 1 };
-	static final int Y[] = { -1, 1, 0, 0, -1, 1, 1, -1 };
-	static int N, M, K, A[][], add[][], cnt, ans;
-	static PriorityQueue<Tree> pq = new PriorityQueue<>();
-	static Queue<Tree> q = new LinkedList<>();
+public class test01 {
+	static final int X[] = { 0, 0, -1, 1 };
+	static final int Y[] = { -1, 1, 0, 0 };
+	static int N, L, R, a[][], cnt, score, ans;
+	static boolean check[][], moving[][];
+	static Queue<Pair> q = new LinkedList<>();;
+	static Queue<Pair> cp;
 
-	static void spring() {
-		if (cnt == K) {
-			while (!pq.isEmpty()) {
-				Tree t = pq.remove();
-				if (t.life == "Y") {
-					ans++;
+	static void bfs() {
+		cp = new LinkedList<>();
+		check = new boolean[N][N];
+		moving = new boolean[N][N];
+		q.add(new Pair(0, 0));
+		score = 0;
+		cnt = 0;
+		while (!q.isEmpty()) {
+			Pair p = q.remove();
+			int x = p.x;
+			int y = p.y;
+			if (check[x][y]) {
+				continue;
+			}
+			check[x][y] = true;
+			for (int k = 0; k < 4; k++) {
+				int nx = x + X[k];
+				int ny = y + Y[k];
+				if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+					continue;
 				}
-			}
-			return;
-		}
-		int size = pq.size();
-		if (size == 0) {
-			return;
-		}
-		for (int s = 0; s < size; s++) {
-			Tree t = pq.remove();
-			int x = t.x;
-			int y = t.y;
-			int age = t.age;
-			System.out.println("cnt:"+cnt+"  "+"K:"+K+"     "+age + " " + t.life + "    ");
-			if (age == 5) { //여기서 중복 발견 안됨.
-				System.out.println(age + " " + t.life + "    " + cnt);
-			}
-			if (A[x][y] >= age) {
-				A[x][y] -= age;
-				pq.add(new Tree(x, y, age + 1, "Y"));
-			} else {
-				pq.add(new Tree(x, y, age, "N"));
-			}
-		}
-		summer();
-	}
-
-	static void summer() {
-		int size = pq.size();
-		if (size == 0) {
-			return;
-		}
-		for (int s = 0; s < size; s++) {
-			Tree t = pq.remove();
-			int x = t.x;
-			int y = t.y;
-			int age = t.age;
-			if (age == 5) { //여기서 중복 발견됨. (죽어야 하는 5가 안죽고 다시 생겨남)
-				System.out.println(age + " " + t.life + "    " + cnt);
-			}
-			String life = t.life;
-			if (life == "N") {
-				A[x][y] += age / 2;
-			} else {
-				q.add(new Tree(x, y, age, "Y"));
-			}
-		}
-		fall();
-	}
-
-	static void fall() {
-		int size = q.size();
-		if (size == 0) {
-			return;
-		}
-		for (int s = 0; s < size; s++) {
-			Tree t = q.remove();
-			int x = t.x;
-			int y = t.y;
-			int age = t.age;
-			if (age % 5 == 0) {
-				for (int k = 0; k < 8; k++) {
-					int nx = x + X[k];
-					int ny = y + Y[k];
-					if (nx < 1 || ny < 1 || nx >= N + 1 || ny >= N + 1) {
-						continue;
+				int calc = Math.abs(a[x][y] - a[nx][ny]);
+				if (calc >= L && calc <= R && !moving[nx][ny]) {
+					moving[nx][ny] = true;
+					if (!moving[x][y]) {
+						cp.add(new Pair(x, y));
+						moving[x][y] = true;
 					}
-					pq.add(new Tree(nx, ny, 1, "Y"));
+					cp.add(new Pair(nx, ny));
+				}
+				q.add(new Pair(nx, ny));
+			}
+		}
+		while (!cp.isEmpty()) {
+			Pair p = cp.remove();
+			int x = p.x;
+			int y = p.y;
+			cnt++;
+			score += a[x][y];
+			a[x][y] = -1;
+		}
+		if (cnt == 0) {
+			return;
+		} else {
+			int val = score / cnt;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (a[i][j] == -1) {
+						a[i][j] = val;
+					}
 				}
 			}
-
-			pq.add(new Tree(x, y, age, "Y"));
-		}
-		winter();
-	}
-
-	static void winter() {
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				A[i][j] += add[i][j];
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					System.out.print(a[i][j] + " ");
+				}
+				System.out.println();
 			}
+			System.out.println();
+			ans++;
+			bfs();
 		}
-		cnt++;
-		spring();
 	}
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt(); // 땅 크기
-		M = sc.nextInt(); // M 개의 나무
-		K = sc.nextInt(); // K 년
-		A = new int[N + 1][N + 1]; // 양분의 값
-		add = new int[N + 1][N + 1];
-		cnt = 0;
+		N = sc.nextInt();
+		L = sc.nextInt();
+		R = sc.nextInt();
+		a = new int[N][N];
 		ans = 0;
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				add[i][j] = sc.nextInt();
-				A[i][j] = 5;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				a[i][j] = sc.nextInt();
 			}
 		}
-		for (int i = 0; i < M; i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			int age = sc.nextInt();
-			pq.add(new Tree(x, y, age, "Y"));
-		}
-		spring();
+		bfs();
 		System.out.println(ans);
 		sc.close();
 	}
 }
-
-아래 테스트케이스는 에러 발견 테스트케이스
-5 2 3
-2 3 2 3 2
-2 3 2 3 2
-2 3 2 3 2
-2 3 2 3 2
-2 3 2 3 2
-2 1 3
-3 2 3
