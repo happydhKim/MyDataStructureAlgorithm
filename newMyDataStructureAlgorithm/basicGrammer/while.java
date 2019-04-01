f0497881d8e68ce91878b2db8e891892a578d3e2
DHKIM <happytime870616@gmail.com>
2019-04-02 00:15:52 +0900

    Algorithm 1.sort -> quick sort update

---
+++ newMyDataStructureAlgorithm/basicGrammer/while.java
@@ -0,0 +1,25 @@
+// 자꾸 헷갈려 매번!!!!!!!!!!!!!!!!!!!!
+
+public class while {
+	public static void main(String args[]) {
+		boolean act = false;
+		while(!act) { // act 가 false 이면 계속 돌아요!!
+			for (int i = 0; i < 10; i++) {
+				System.out.println(i);
+
+			}
+			act = true;
+		}
+
+    int a = 2;
+		while(a>0) { // a 가 0보다 크면 계속 돌아요!!!
+			for (int i = 0; i < 10; i++) {
+				System.out.println(i);
+
+			}
+			a--;
+		}
+
+
+	}
+}
--- newMyDataStructureAlgorithm/sort/quickSort.java
+++ newMyDataStructureAlgorithm/sort/quickSort.java
@@ -1,3 +1,5 @@
+// 퀵 정렬은 불안정 정렬 에 속하며, 다른 원소와의 비교만으로 정렬을 수행하는 비교 정렬 에 속한다.
+
 import java.util.*;
 
 public class quickSort {
