6fb6ca36dfd8b7aba15086fe32e7fecfdf40a920
DESKTOP-TB730O5\happy <happytime870616@gmail.com>
2019-04-08 09:26:14 +0900

    Algorithm 1.update fail problem (fail)

--- newMyDataStructureAlgorithm/solve/boj/dfs/17136.java
+++ newMyDataStructureAlgorithm/solve/boj/dfs/17136.java
@@ -1,97 +1,94 @@
-package onlineStudy;
+package dss;
 
-//fail
 import java.util.Scanner;
 
-public class goDs2 {
-	static int a[][], ans;
-	static boolean write;
+public class test03 {
+	static final int N = 10;
+	static int a[][], things[], ans;
+	static boolean check[][], write;
 
-	static void dfs(int x, int y, int things[], int cnt, boolean check[][]) {
-		// System.out.println(x + " " + y);
-		if (x == 9 && y == 10) {// end
-			for (int i = 0; i < 10; i++) {
-				for (int j = 0; j < 10; j++) {
-					if (a[i][j] == 1 && !check[i][j]) {
-						System.out.println(cnt);
-						return;
-					}
+	static boolean possible(int x, int y, int size) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (a[i + x][j + y] != 1 || check[i + x][j + y]) {
+					return false;
 				}
 			}
+		}
+		return true;
+	}
+
+	static void chkTrue(int x, int y, int size) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				check[i + x][j + y] = true;
+			}
+		}
+	}
+
+	static void chkFalse(int x, int y, int size) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				check[i + x][j + y] = false;
+			}
+		}
+	}
+
+	static boolean chkEnd() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (a[i][j] == 1 && !check[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	static void dfs(int cnt) {
+		if (ans <= cnt) {
+			return;
+		}
+		if (chkEnd()) {
+			System.out.println(cnt);
 			if (ans > cnt) {
 				ans = cnt;
-				System.out.println(ans + "answer");
-				return;
 			}
+			return;
 		}
-		if (y == 10) {
-			x++;
-			y = 0;
-		}
-		if (a[x][y] == 1 && !check[x][y]) {
-			for (int i = 0; i < 5; i++) {
-				if (x + i >= 10 || y + i >= 10) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (a[i][j] == 0) {
 					continue;
 				}
-				write = false;
-				if (things[i] == 0) {
-					continue;
-				} else {
-					things[i] -= 1;
-				}
-
-				boolean possible = true;
-				int size = i + 1;
-				for (int q = 0; q < size; q++) {
-					if (!possible) {
-						break;
-					}
-					for (int j = 0; j < size; j++) {
-						if (x + q >= 10 || y + j >= 10 || check[x + q][y + j] || a[x + q][y + j] != 1) {
-							possible = false;
-							break;
+				System.out.println(i + " " + j);
+				for (int k = 5; k >= 1; k--) {
+					if (a[i][j] == 1 && things[k] < 6 && i + k < N && j + k < N) {
+						if (possible(i, j, k)) {
+							chkTrue(i, j, k);
+							things[k] += 1;
+							dfs(cnt + 1);
+							chkFalse(i, j, k);
+							things[k] -= 1;
 						}
 					}
 				}
-				if (possible) {
-					write = true;
-					for (int q = 0; q < size; q++) {
-						for (int j = 0; j < size; j++) {
-							check[x + q][y + j] = true;
-						}
-					}
-				}
-				if (!write) {
-					continue;
-				}
-				dfs(x, y + 1, things, cnt + 1, check);
-				things[i] += 1;
-				for (int q = 0; q < size; q++) {
-					for (int j = 0; j < size; j++) {
-						check[x + q][y + j] = false;
-					}
-				}
 			}
-		} else {
-			dfs(x, y + 1, things, cnt, check);
 		}
 	}
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		a = new int[10][10];
+		a = new int[N][N];
 		ans = 987654321;
-		boolean check[][] = new boolean[10][10];
-		int things[] = new int[5];
-		for (int i = 0; i < 5; i++) {
-			things[i] = 5;
-		}
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
+		check = new boolean[N][N];
+		things = new int[6];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
 				a[i][j] = sc.nextInt();
 			}
 		}
-		dfs(0, 0, things, 0, check);
+		dfs(0);
 		if (ans == 987654321) {
 			ans = -1;
 		}
