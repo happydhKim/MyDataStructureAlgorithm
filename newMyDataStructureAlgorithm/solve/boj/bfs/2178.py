66faddb2ef67bb810715d37f776a1218846d847c
happydhKim <happytime870616@gmail.com>
2019-04-26 17:14:56 +0900

    Algorithm 1. update solved problem ->bfs

--- newMyDataStructureAlgorithm/solve/boj/bfs/2178.py
+++ newMyDataStructureAlgorithm/solve/boj/bfs/2178.py
@@ -1,3 +1,5 @@
+#case1
+
 from sys import stdin
 
 def bfs(start,goal):
@@ -27,3 +29,41 @@ for i in range(N):
 start = (0,0)
 goal = (N-1,M-1)
 print(bfs(start,goal))
+
+
+
+#case2
+
+from sys import*
+input = stdin.readline
+setrecursionlimit(10**6)
+
+def bfs(y,x):
+    q=[]
+    q.append([y,x])
+    while q:
+        y,x=q.pop(0)
+        if y==n-1 and x==m-1 :
+            return visit[y][x]
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if (ny<0 or nx<0 or ny>n-1 or nx>m-1 or arr[ny][nx]=='0'
+                or visit[ny][nx]!=0):
+                continue
+
+            visit[ny][nx]=visit[y][x]+1
+            q.append([ny,nx])
+
+    return -1
+
+n,m=map(int,input().split())
+arr=[]
+visit=[[0]*m for i in range(n)]
+visit[0][0]=1
+for i in range(n):
+    arr.append(input().strip())
+dy=[-1,1,0,0]
+dx=[0,0,1,-1]
+
+print(bfs(0,0))
