9aeac8c7dd998153d2bddf1385b5b9962e17ef0c
happydhKim <happytime870616@gmail.com>
2019-05-15 13:53:47 +0900

    problem 1. update solve problem

--- newMyDataStructureAlgorithm/solve/temp.java
+++ newMyDataStructureAlgorithm/solve/temp.java
@@ -1,34 +1,38 @@
 import java.util.Scanner;
 
 public class temp {
-	static int N, ans;
-
-	static void dfs(int d) {
-		if (d == N / 2) {
-			System.out.println(d);
-			return;
-		}
-		int temp = d;
-		int cnt = d;
-		while (temp < N / 2) {
-			System.out.println("Asd");
-			if (cnt == N) {
-				ans++;
-			}
-			temp++;
-			cnt += temp;
-		}
-		dfs(d+1);
-	}
+	static String answer;
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
+		sc.nextLine();
 		for (int TC = 1; TC <= T; TC++) {
-			N = sc.nextInt();
-			ans = 0;
-			dfs(1);
-			System.out.println("#" + TC + " " + ans);
+			String s = sc.nextLine();
+			String str[] = s.split(" ");
+			answer = "SAME";
+			if (str[0].length() != str[1].length()) {
+				answer = "DIFF";
+			} else {
+				for (int i = 0; i < str[0].length(); i++) {
+					char c = str[0].charAt(i);
+					char c2 = str[1].charAt(i);
+					if (c == 'B') {
+						if (c2 != 'B') {
+							answer = "DIFF";
+							break;
+						}
+					} else {
+						if (c == 'A' || c == 'D' || c == 'O' || c == 'P' || c == 'Q' || c == 'R') {
+							if (c2 != 'A' && c2 != 'D' && c2 != 'O' && c2 != 'P' && c2 != 'Q' && c2 != 'R') {
+								answer = "DIFF";
+								break;
+							}
+						}
+					}
+				}
+			}
+			System.out.println("#" + TC + " " + answer);
 		}
 		sc.close();
 	}
