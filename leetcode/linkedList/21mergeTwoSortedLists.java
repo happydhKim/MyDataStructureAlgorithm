35e54156387e5e1b00a8f53b275c771aed406859
happydhKim <happytime870616@gmail.com>
2019-06-20 18:11:19 +0900

    leetcode 1.linked list -> problem number 21

---
+++ leetcode/linkedList/21mergeTwoSortedLists.java
@@ -0,0 +1,36 @@
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null && l2 == null) return null;
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+
+        ListNode dummy = new ListNode(0);
+        ListNode ans = dummy;
+
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                dummy.next = l1;
+                l1 = l1.next;
+                dummy = dummy.next;
+            } else {
+                dummy.next = l2;
+                l2 = l2.next;
+                dummy = dummy.next;
+            }
+        }
+        if(l1 != null) {
+            dummy.next = l1;
+        }
+        if(l2 != null) {
+            dummy.next = l2;
+        }
+        return ans.next;
+    }
+}
