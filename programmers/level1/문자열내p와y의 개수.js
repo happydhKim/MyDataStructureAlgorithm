2479771a911135a33a05792048557fbb9d6bd72e
happydhKim <happytime870616@gmail.com>
2021-02-01 21:26:16 +0900

    [Programmers] 새로운 방식으로 문제 수정

--- programmers/level1/문자열내p와y의 개수.js
+++ programmers/level1/문자열내p와y의 개수.js
@@ -1,14 +1,5 @@
 function solution(s){
   const str = s.toUpperCase();
 
-  let pCount = 0;
-  let yCount = 0;
-  for (let i = 0; i < str.length; i++) {
-
-    if(str[i] === 'P') pCount++;
-    if(str[i] === 'Y') yCount++;
-  }
-  return pCount === yCount ? true : false;
+  return str.split('P').length === str.split('Y').length;
 }
-
-console.log(solution('pPoooyY'));
