7452644f0dd4437f7177930e983a41e4e466b6fc
happydhKim <happytime870616@gmail.com>
2021-02-25 22:25:54 +0900

    [Algorithm] Permutaion N과 M (1)

---
+++ algorithm/N과M/N과M1.js
@@ -0,0 +1,26 @@
+// https://www.acmicpc.net/problem/15649
+const [N, M] = require('fs').readFileSync('dev/stdin').toString().split(' ').map(x => + x);
+
+let check = new Array(N).fill(false);
+let array = [];
+let output = '';
+
+const dfs = (depth) => {
+  if(depth >= M){
+    output += array.join(' ') + '\n';
+    
+    return;
+  }
+  for(let i = 1; i <= N; i++) {
+    if (!check[i]) {
+      check[i] = true;
+      array.push(i);
+      dfs(depth + 1);
+      array.pop();
+      check[i] = false;
+    }
+  }
+}
+
+dfs(0);
+console.log(output);
