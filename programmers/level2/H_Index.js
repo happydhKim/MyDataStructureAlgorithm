c3a752d3b4a811a144e9312d6f16b1f3e0db501f
happydhKim <happytime870616@gmail.com>
2021-03-08 23:48:10 +0900

    [Programmers] Level2 H-index

---
+++ programmers/level2/H_Index.js
@@ -0,0 +1,15 @@
+function solution(citations) {
+  let answer = 0;
+
+  citations.sort((a, b) => b - a); 
+
+  while(answer <= citations.length) {
+    if (answer + 1 <= citations[answer]) {
+      answer++;   
+    } else {
+      break;
+    }
+  }
+
+  return answer;
+}
